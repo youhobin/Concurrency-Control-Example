6f9b29c524388b961304c57d075df9dfa6429f89
ghdb132 <ghdb132@naver.com>
2024-04-26 17:05:22 +0900

    init 동시성 제어 공부용 프로젝트 setting

---
+++ src/main/java/com/example/stockexample/StockExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stockexample/StockExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
